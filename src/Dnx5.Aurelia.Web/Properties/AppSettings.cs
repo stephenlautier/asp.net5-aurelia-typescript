a94ee888fe7d4519815dc1dbd185c5ee5bb61f3e
stephen.lautier <stephen.lautier@outlook.com>
2015-06-20 00:40:30 +0200

    initial project commit

---
+++ src/Dnx5.Aurelia.Web/Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dnx5.Aurelia.Web
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
