5c8e2eafe3258e9857c22922cabc6bdde52de822
stephen.lautier <stephen.lautier@outlook.com>
2015-06-20 21:32:17 +0200

    gulp:  - sass compile  - browser sync  - changes in copy  - watch

--- src/Dnx5.Aurelia.Web/gulpfile.js
+++ src/Dnx5.Aurelia.Web/gulpfile.js
@@ -1,21 +1,59 @@
-﻿/// <binding Clean='clean' />
+﻿/// <binding Clean='clean' ProjectOpened='watch' />
 
 var gulp = require("gulp"),
-	rimraf = require("rimraf"),
-	fs = require("fs");
+	del = require("del"),
+	runseq = require("run-sequence"),
+	fs = require("fs"),
+	browserSync = require("browser-sync"),
+	sass = require("gulp-sass"),
+	sourcemaps = require("gulp-sourcemaps");
 
 eval("var project = " + fs.readFileSync("./project.json"));
 
 var paths = {
 	bower: "./bower_components/",
-	lib: "./" + project.webroot + "/lib/"
+	lib: "./" + project.webroot + "/lib/",
+	sass: {
+		src: ["./assets/styles/**/*.scss"],
+		dest: "./" + project.webroot + "/styles/"
+	},
+	publicRoot: "./" + project.webroot
 };
 
-gulp.task("clean", function(cb) {
-	rimraf(paths.lib, cb);
+// ** Watching ** //
+
+gulp.task("watch", ["serve"], function () {
+
+	gulp.watch(paths.sass.src, ["compile:sass", browserSync.reload]).on("change", reportChange);
+
 });
 
-gulp.task("copy", ["clean"], function() {
+// ** Compile ** //
+
+gulp.task("build", ["clean"], function (cb) {
+
+	runseq(["compile:sass", "copy"], cb);
+
+});
+
+
+gulp.task("compile:sass", function () {
+	gulp.src(paths.sass.src)
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(paths.sass.dest));
+
+});
+
+
+
+gulp.task("clean", function (cb) {
+	del([paths.lib, paths.sass.dest], cb);
+
+});
+
+gulp.task("copy", function () {
 	var bower = {
 		"bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}",
 		"bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/**/*.{js,css}",
@@ -30,3 +68,38 @@ gulp.task("copy", ["clean"], function() {
 			.pipe(gulp.dest(paths.lib + destinationDir));
 	}
 });
+
+// ** Serve ** //
+
+gulp.task("serve", function (cb) {
+
+	//browserSync({
+	//	open: false,
+	//	port: 9000,
+	//	server: {
+	//		baseDir: [paths.publicRoot],
+	//		middleware: function (req, res, next) {
+	//			res.setHeader("Access-Control-Allow-Origin", "*");
+	//			next();
+	//		}
+	//	}
+	//}, cb);
+	browserSync({
+		//open: false,
+		// Using a localhost address with a port
+		//proxy: "localhost:37084"
+		// Proxy with middleware
+		//proxy: {
+		//	target: "localhost:37084",
+		//	middleware: function (req, res, next) {
+		//		//console.log(req.url);
+		//		next();
+		//	}
+		//}
+	}, cb);
+
+});
+
+function reportChange(event) {
+	console.log("File " + event.path + " was " + event.type + ", running tasks...");
+}
