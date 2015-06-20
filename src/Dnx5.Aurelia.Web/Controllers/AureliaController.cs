663659fe0815365d5308fdc7dc98e0b57944f1d6
stephen.lautier <stephen.lautier@outlook.com>
2015-06-20 19:12:58 +0200

    added SASS content + created aurelia controller + Layout for SPA

---
+++ src/Dnx5.Aurelia.Web/Controllers/AureliaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Dnx5.Aurelia.Web.Controllers
+{
+	public class AureliaController : Controller
+	{
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- src/Dnx5.Aurelia.Web/Startup.cs
+++ src/Dnx5.Aurelia.Web/Startup.cs
@@ -68,7 +68,7 @@ namespace Dnx5.Aurelia.Web
 			{
 				routes.MapRoute(
 					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
+					template: "{controller=Aurelia}/{action=Index}/{id?}");
 
 				// Uncomment the following line to add a route for porting Web API 2 controllers.
 				// routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
