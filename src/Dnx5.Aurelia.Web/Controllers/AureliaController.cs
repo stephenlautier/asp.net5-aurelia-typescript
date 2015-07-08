fb581ec52198cabc59c75bfa47aa1b48c31e71d3
stephen.lautier <stephen.lautier@outlook.com>
2015-07-08 02:16:58 +0200

    dnx migration from beta4 to beta5

--- src/Dnx5.Aurelia.Web/Controllers/AureliaController.cs
+++ src/Dnx5.Aurelia.Web/Controllers/AureliaController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.OptionsModel;
 
 namespace Dnx5.Aurelia.Web.Controllers
 {
 	public class AureliaController : Controller
 	{
+		private readonly IOptions<AppSettings> _appSettings;
+
+		public AureliaController(IOptions<AppSettings> appSettings)
+		{
+			_appSettings = appSettings;
+			if (appSettings == null)
+			{
+				
+			}
+		}
+
 		// GET: /<controller>/
 		public IActionResult Index()
 		{
+			if(_appSettings == null) {}
+
 			return View();
 		}
 	}
--- src/Dnx5.Aurelia.Web/Startup.cs
+++ src/Dnx5.Aurelia.Web/Startup.cs
@@ -7,21 +7,25 @@ using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Routing;
-using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
 using Microsoft.Framework.Logging.Console;
+using Microsoft.Framework.Runtime;
 
 namespace Dnx5.Aurelia.Web
 {
 	public class Startup
 	{
-		public Startup(IHostingEnvironment env)
+		public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
 		{
+			
 			// Setup configuration sources.
-			Configuration = new Configuration()
+			var configBuilder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
 				.AddJsonFile("config.json")
 				.AddEnvironmentVariables();
+
+			Configuration = configBuilder.Build();
 		}
 
 		public IConfiguration Configuration { get; set; }
@@ -29,7 +33,7 @@ namespace Dnx5.Aurelia.Web
 		// This method gets called by the runtime.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.Configure<AppSettings>(Configuration.GetSubKey("AppSettings"));
+			services.Configure<AppSettings>(Configuration.GetConfigurationSection("AppSettings"));
 
 			// Add MVC services to the services container.
 			services.AddMvc();
@@ -50,7 +54,7 @@ namespace Dnx5.Aurelia.Web
 			// Add the following to the request pipeline only in development environment.
 			if (env.IsEnvironment("Development"))
 			{
-				app.UseBrowserLink();
+				//app.UseBrowserLink();
 				app.UseErrorPage(ErrorPageOptions.ShowAll);
 			}
 			else
